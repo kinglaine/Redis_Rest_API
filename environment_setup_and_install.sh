a907fa9714bc5f2b440d33a662410ca5e5a3c3d9
yuzchen7 <yuzchen7@gmail.com>
2022-11-22 20:44:19 -0500

    init: linux environment setup

--- environment_setup_and_install.sh
+++ environment_setup_and_install.sh
@@ -1,17 +1,30 @@
 #! /bin/bash
 
 ####################################################
-# Function :download and install redis             #
+# Function :download and install Environment       #
 # Platform :linux Platform only                    #
 # Version  :1.0                                    #
 # Date     :11/26/2022                             #
 # Author   :Yuzhuang Chen (yuz.chen)               #
 ####################################################
+apt-get update;
 
 which redis-server;
-
 if [ $? -ne 0 ] 
 then
-    apt-get update;
     apt-get install redis;
 fi
+
+which node;
+if [ $? -ne 0 ] 
+then
+    apt-get install nodejs;
+fi
+
+which npm;
+if [ $? -ne 0 ] 
+then
+    apt-get install npm;
+fi
+npm install express;
+npm install ioredis;
